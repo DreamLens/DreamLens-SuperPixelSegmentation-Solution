368a3efa7552862ee5869e9915032d87e8ee762f
DreamLens <DreamLen98@proton.me>
2020-03-13 20:10:07 +0000

    cuda

--- GUI/HelpersQt.cpp
+++ GUI/HelpersQt.cpp
@@ -1,3 +1,6 @@
 /*=========================================================================
  *
- *  Copyright David Doria 2011 davidd
+ *  Copyright David Doria 2011 daviddoria@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file ex
