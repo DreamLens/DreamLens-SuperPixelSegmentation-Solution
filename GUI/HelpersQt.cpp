fec217da70148b6f5a119542a0c68646ac2c2bbb
DreamLens <DreamLen98@proton.me>
2020-03-14 16:11:23 +0000

    #86

--- GUI/HelpersQt.cpp
+++ GUI/HelpersQt.cpp
@@ -6,4 +6,7 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *         http://www.apache.org
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ 
