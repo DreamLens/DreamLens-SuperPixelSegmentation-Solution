e11a675e31213208228c2a49453a8bc4db56a8d1
DreamLens <DreamLen98@proton.me>
2020-04-01 18:36:43 +0000

    added padding

--- GUI/HelpersQt.hxx
+++ GUI/HelpersQt.hxx
@@ -14,4 +14,9 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *====================================================================
+ *=========================================================================*/
+
+// ITK
+#include "itkRegionOfInterestImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#
