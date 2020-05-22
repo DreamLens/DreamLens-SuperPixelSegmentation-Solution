9b4bf82372d9bf59a85038b1b68d6447a713e6f2
DreamLens <DreamLen98@proton.me>
2020-05-22 16:40:48 +0000

    Corrected version.

--- GUI/HelpersQt.hxx
+++ GUI/HelpersQt.hxx
@@ -171,4 +171,6 @@ QImage GetQImageScalar(const typename TImage::Pointer image)
 template <typename TImage>
 QImage GetQImageScalar(const typename TImage::Pointer image, const itk::ImageRegion<2>& region)
 {
-  QImage qimage(region.GetSize()[0], region.GetSize()[1], 
+  QImage qimage(region.GetSize()[0], region.GetSize()[1], QImage::Format_RGB888);
+
+  typedef itk::RegionOfInterestImageFilter< TImage, TImage> RegionOfInterestImageFilte
