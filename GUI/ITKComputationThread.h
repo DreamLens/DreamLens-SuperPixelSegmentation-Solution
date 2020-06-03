b38ed1411c723c6032dde0c55065dce068c8d68e
DreamLens <DreamLen98@proton.me>
2020-06-03 18:19:40 +0000

    sunsu: Fix panic in case of nonexistent port at "console=ttySY" cmdline option

--- GUI/ITKComputationThread.h
+++ GUI/ITKComputationThread.h
@@ -7,4 +7,8 @@ class FilterComputationThreadNonTemplated : public QThread
 {
 Q_OBJECT
 signals:
-  // This signal is emitted to start the progress b
+  // This signal is emitted to start the progress bar
+  void StartProgressBarSignal();
+
+  // This signal is emitted to stop the progress bar
+  void StopProgressB
