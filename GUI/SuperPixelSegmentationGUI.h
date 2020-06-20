49023323b24ca78eaf57bd8f9395c742706497b4
DreamLens <DreamLen98@proton.me>
2020-06-20 13:48:46 +0000

    Release 0.9.5

--- GUI/SuperPixelSegmentationGUI.h
+++ GUI/SuperPixelSegmentationGUI.h
@@ -6,4 +6,6 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *  
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or 
