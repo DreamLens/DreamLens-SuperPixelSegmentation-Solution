05ba432608933c1a8aa90c57aa04e31202658c9f
DreamLens <DreamLen98@proton.me>
2020-07-28 17:56:59 +0000

    Version 0.15.0

--- GUI/Widgets/FloatSlider.h
+++ GUI/Widgets/FloatSlider.h
@@ -7,4 +7,6 @@ class QDoubleValidator;
 #include "LabeledSlider.h"
 
 // This slider should always be set from min = 0 to max = 100.
-// This then le
+// This then lets us interpret the difference between the min and max
+// as a percentage.
+class QDESIGNER_WIDGET_EXPORT FloatSlider : p
