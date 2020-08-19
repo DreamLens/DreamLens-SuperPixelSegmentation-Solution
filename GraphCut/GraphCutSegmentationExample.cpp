87a311562f3369920fb447dc4949703e28ef3bc2
DreamLens <DreamLen98@proton.me>
2020-08-19 08:25:28 +0000

    activity

---
+++ GraphCut/GraphCutSegmentationExample.cpp
@@ -0,0 +1,34 @@
+
+#include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+#include "itkGraphCutSegmentation.h"
+
+//typedef itk::Image<itk::CovariantVector<float, 3>, 2> ImageType;
+typedef itk::VectorImage<float, 2> ImageType;
+typedef itk::Image<int, 2> LabelImageType;
+
+int main(int argc, char* argv[])
+{
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName(argv[1]);
+  reader->Update();
+  
+  typedef itk::GraphCutSegmentation<ImageType, LabelImageType> GraphCutSegmentationType;
+  GraphCutSegmentationType::Pointer graphCutSegmentation = GraphCutSegmentationType::New();
+  graphCutSegmentation->SetSigma(.5);
+  graphCutSegmentation->SetK(500);
+  graphCutSegmentation->SetMinSize(50);
+  graphCutSegmentation->SetInput(reader->GetOutput());
+  graphCutSegmentation->Update();
+  
+  typedef itk::ImageFileWriter<LabelImageType> WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName(argv[2]);
+  writer->SetInput(graphCutSegmentation->GetOutput());
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
