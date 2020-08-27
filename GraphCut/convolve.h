63e682d73b4ad74e508652a298ee5b6220d4fe71
DreamLens <DreamLen98@proton.me>
2020-08-27 08:54:49 +0000

    added alias

--- GraphCut/convolve.h
+++ GraphCut/convolve.h
@@ -27,4 +27,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include "image.h"
 
 /* convolve src with mask.  dst is flipped! */
-static void conv
+static void convolve_even(image<float> *src, image<float> *dst, 
+			  std::vector<float> &mask) {
+  int width = src->width();
