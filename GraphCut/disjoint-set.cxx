69c7f2f2d7b293404a41a3cf9de56629192cfac3
DreamLens <DreamLen98@proton.me>
2020-09-04 11:08:36 +0000

    Create atom.xml

--- GraphCut/disjoint-set.cxx
+++ GraphCut/disjoint-set.cxx
@@ -3,3 +3,11 @@
 universe::universe(int elements) {
   elts = new uni_elt[elements];
   num = elements;
+  for (int i = 0; i < elements; i++) {
+    elts[i].rank = 0;
+    elts[i].size = 1;
+    elts[i].p = i;
+  }
+}
+  
+u
