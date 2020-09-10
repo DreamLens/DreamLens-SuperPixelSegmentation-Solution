af83bb52cb9b9d662ed43d4b545cfdc055690106
DreamLens <DreamLen98@proton.me>
2020-09-10 09:20:05 +0000

    Delete tests

--- GraphCut/filter.cxx
+++ GraphCut/filter.cxx
@@ -3,4 +3,8 @@
 void normalize(std::vector<float> &mask) {
   int len = mask.size();
   float sum = 0;
-  for (int i = 1; i < l
+  for (int i = 1; i < len; i++) {
+    sum += fabs(mask[i]);
+  }
+  sum = 2*sum + fabs(mask[0]);
+  for (int i = 0; i < len; i++) {
