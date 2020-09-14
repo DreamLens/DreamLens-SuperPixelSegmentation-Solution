c9d08cfc7b4d8bf5bab7089c85828587bb9bcab1
DreamLens <DreamLen98@proton.me>
2020-09-14 08:50:41 +0000

    Update solution.cpp

--- GraphCut/filter.cxx
+++ GraphCut/filter.cxx
@@ -21,4 +21,11 @@ image<float> *smooth(image<float> *src, float sigma) {
   image<float> *tmp = new image<float>(src->height(), src->width(), false);
   image<float> *dst = new image<float>(src->width(), src->height(), false);
   convolve_even(src, tmp, mask);
-  convolve_even(tmp, dst, 
+  convolve_even(tmp, dst, mask);
+
+  delete tmp;
+  return dst;
+}
+
+/* convolve image with gaussian filter */
+image<float> *smo
