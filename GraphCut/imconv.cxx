bb8869db7884f96119efe1513fbc95bf9c6b0066
DreamLens <DreamLen98@proton.me>
2020-09-24 21:58:10 +0000

    updat readme

--- GraphCut/imconv.cxx
+++ GraphCut/imconv.cxx
@@ -3,4 +3,7 @@
 image<uchar> *imageRGBtoGRAY(image<rgb> *input) {
   int width = input->width();
   int height = input->height();
-  image<uc
+  image<uchar> *output = new image<uchar>(width, height, false);
+
+  for (int y = 0; y < height; y++) {
+    for (
