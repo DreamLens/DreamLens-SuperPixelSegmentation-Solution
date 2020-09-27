5d5cd25936e3796d7eaaa079918f565d7a835101
DreamLens <DreamLen98@proton.me>
2020-09-27 22:08:49 +0000

    codestyle fixes

--- GraphCut/imconv.cxx
+++ GraphCut/imconv.cxx
@@ -22,4 +22,6 @@ image<rgb> *imageGRAYtoRGB(image<uchar> *input) {
   image<rgb> *output = new image<rgb>(width, height, false);
 
   for (int y = 0; y < height; y++) {
-    for (int x = 
+    for (int x = 0; x < width; x++) {
+      imRef(output, x, y).r = imRef(input, x, y);
+      imRef(output, x, y).g = imRef(input, x, 
