920aa4e0555a1c2852df2d8c591ce97f4dd357e8
DreamLens <DreamLen98@proton.me>
2020-11-04 05:27:14 +0000

    update cores

--- GraphCut/imconv.cxx
+++ GraphCut/imconv.cxx
@@ -132,4 +132,11 @@ image<uchar> *imageSHORTtoUCHAR(image<short> *input,
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       uchar val = (uchar)((imRef(input, x, y) - min) * scale);
-      imRef(output, x, y) = bound(val, (uchar)0, (uchar)UCHAR
+      imRef(output, x, y) = bound(val, (uchar)0, (uchar)UCHAR_MAX);
+    }
+  }
+  return output;
+}
+
+image<uchar> *imageSHORTtoUCHAR(image<short> *input) {
+ 
