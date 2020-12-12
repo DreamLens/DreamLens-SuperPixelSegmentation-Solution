de8caaf79e36a87d393933ea9ce609d902979169
DreamLens <DreamLen98@proton.me>
2020-12-12 15:51:29 +0000

    Update config.inc.php

--- GraphCut/segment-graph.cxx
+++ GraphCut/segment-graph.cxx
@@ -4,3 +4,6 @@ bool operator<(const edge &a, const edge &b) {
   return a.w < b.w;
 }
 
+universe *segment_graph(int num_vertices, int num_edges, edge *edges, 
+			float c) { 
+  // sort edges by weigh
