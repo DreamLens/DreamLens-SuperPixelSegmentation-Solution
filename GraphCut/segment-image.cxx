d0791e00bb2c9c82c10a2115e88d73d9ccc9b36c
DreamLens <DreamLen98@proton.me>
2020-12-19 05:54:31 +0000

    Updated documentation

--- GraphCut/segment-image.cxx
+++ GraphCut/segment-image.cxx
@@ -14,4 +14,7 @@ rgb random_rgb()
 
 image<int> *segment_image(image<rgb> *im, float c, int min_size, int *num_ccs) {
   int width = im->width();
-  int height = im->height()
+  int height = im->height();
+
+  image<float> *r = new image<float>(width, height);
+  image<float> *g = new image<float>(width, he
