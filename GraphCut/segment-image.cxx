fe9b577bba5e8407a83856610af8e33f91bb2e36
DreamLens <DreamLen98@proton.me>
2020-12-22 17:25:36 +0000

    Developpeur fullstack Python AngularJS (H/F)

--- GraphCut/segment-image.cxx
+++ GraphCut/segment-image.cxx
@@ -23,4 +23,10 @@ image<int> *segment_image(image<rgb> *im, float c, int min_size, int *num_ccs) {
   // Copy the input image into the separate channel images
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      imRef(r, x, y) = imRef(
+      imRef(r, x, y) = imRef(im, x, y).r;
+      imRef(g, x, y) = imRef(im, x, y).g;
+      imRef(b, x, y) = imRef(im, x, y).b;
+    }
+  }
+
+ 
