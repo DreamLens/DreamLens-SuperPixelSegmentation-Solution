a9d324e67ce6b9f55aae50392bd5a1d20f9f812a
DreamLens <DreamLen98@proton.me>
2020-12-31 19:20:32 +0000

    Button styles

--- GraphCut/segment-image.cxx
+++ GraphCut/segment-image.cxx
@@ -57,4 +57,11 @@ image<int> *segment_image(image<rgb> *im, float c, int min_size, int *num_ccs) {
 
       if ((x < width-1) && (y > 0)) {
 	edges[num].a = y * width + x;
-	edges[num].b = (y
+	edges[num].b = (y-1) * width + (x+1);
+	edges[num].w = diff(r, g, b, x, y, x+1, y-1);
+	num++;
+      }
+    }
+  }
+
+  d
