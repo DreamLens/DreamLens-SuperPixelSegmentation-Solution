70a74be8f36d56ccb99d1f3236d64b5aafd8df77
DreamLens <DreamLen98@proton.me>
2021-02-18 15:14:27 +0000

    Adds MIT license

--- Helpers.cpp
+++ Helpers.cpp
@@ -5,4 +5,8 @@ namespace Helpers
   
 // This is a specialization that ensures that the number of pixels per component also matches.
 template<>
-void DeepCopy<itk::VectorImage<unsigned char, 2> >(const itk::VectorImage<unsigned char, 2>* input, itk::VectorImage<unsigned char, 2>* o
+void DeepCopy<itk::VectorImage<unsigned char, 2> >(const itk::VectorImage<unsigned char, 2>* input, itk::VectorImage<unsigned char, 2>* output)
+{
+  //std::cout << "DeepCopy<FloatVectorImageType>()" << std::endl;
+  bool changed = false;
+  if(inpu
