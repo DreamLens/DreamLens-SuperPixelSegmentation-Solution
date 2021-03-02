76ebafa10f4e86588a84a0a1d8b2ea30d008826a
DreamLens <DreamLen98@proton.me>
2021-03-02 19:52:30 +0000

    cert script

--- Helpers.h
+++ Helpers.h
@@ -2,4 +2,5 @@
  *
  *  Copyright David Doria 2011 daviddoria@gmail.com
  *
- *  Licensed under the Apache Licen
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the Lice
