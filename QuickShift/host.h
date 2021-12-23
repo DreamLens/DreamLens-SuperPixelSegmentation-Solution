e294050ac2af4bd26efdb12576f8322a9b6d5712
DreamLens <DreamLen98@proton.me>
2021-12-23 13:19:14 +0000

    Adds .gitignore

--- QuickShift/host.h
+++ QuickShift/host.h
@@ -6,4 +6,8 @@
 /* AUTORIGHTS
 Copyright (C) 2007-10 Andrea Vedaldi and Brian Fulkerson
 
-This file is part of VLFeat, avail
+This file is part of VLFeat, available under the terms of the
+GNU GPLv2, or (at your option) any later version.
+*/
+
+#ifndef VL_HOST_H
