f74481a1c1a9723d68e6cd41449ff48584dfe063
DreamLens <DreamLen98@proton.me>
2022-11-05 14:08:34 +0000

    removed useless line

--- QuickShift/itkQuickShiftSegmentation.hxx
+++ QuickShift/itkQuickShiftSegmentation.hxx
@@ -103,3 +103,109 @@ void QuickShiftSegmentation< TInputImage, TOutputLabelImage>
 
   vl_quickshift_process(quickshift);
   std::cout << "Finished processing." << std::endl;
+
+  // Retrieve the parents (These can be used to segment the image in superpixels.)
+  int* parents = vl_quickshift_get_parents(quickshift);
+
+  std::cout << "GetVectorFromArray()" << std::endl;
+  std::vector<int> parentsVector = GetVectorFromArray(parents, totalPixels);
+
+  std::cout << "GetLabelsFromParents()" << std::endl;
+  std::vector<int> labels = GetLabelsFromParents(parentsVector);
+
+  // Construct label image from label array
+  typename TOutputLabelImage::Pointer outputLabelImage = this->GetLabelImage(); // One of the output ports
+  outputLabelImage->SetRegions(input->GetLargestPossibleRegion());
+  outputLabelImage->Allocate();
+
+  itk::ImageRegionIterator<TOutputLabelImage> labelIterator(outputLabelImage, outputLabelImage->GetLargestPossibleRegion());
+
+  unsigned int labelId = 0;
+  while(!labelIterator.IsAtEnd())
+    {
+    unsigned int linearIndex = ComputeLinearPixelIndex(labelIterator.GetIndex()[1], labelIterator.GetIndex()[0], height);
+    labelIterator.Set(labels[linearIndex]);
+
+    ++labelIterator;
+    labelId++;
+    }
+
+  // Delete the quick shift object
+  vl_quickshift_delete(quickshift);
+
+  std::cout << "RelabelSequential()" << std::endl;
+  Helpers::RelabelSequential<TOutputLabelImage>(outputLabelImage, outputLabelImage); // This is the 0th output port of the filter
+
+  Helpers::WriteImage<TOutputLabelImage>(outputLabelImage, "QuickShift_LabelImage.mha");
+
+  std::cout << "ColorLabelsByAverageColor()" << std::endl;
+  Helpers::ColorLabelsByAverageColor<TInputImage, TOutputLabelImage>(input, this->GetLabelImage(), this->GetColoredImage());
+  Helpers::WriteImage<TInputImage>(this->GetColoredImage(), "QuickShift_ColoredImage.mha");
+}
+
+template< typename TInputImage, typename TOutputLabelImage>
+template<typename T>
+std::vector<T> QuickShiftSegmentation< TInputImage, TOutputLabelImage>
+::GetVectorFromArray(const T* array, const unsigned int size)
+{
+  std::vector<T> myVector(size);
+  for(unsigned int i = 0; i < size; ++i)
+    {
+    myVector[i] = array[i];
+    }
+  return myVector;
+}
+
+template< typename TInputImage, typename TOutputLabelImage>
+std::vector<int> QuickShiftSegmentation< TInputImage, TOutputLabelImage>
+::GetLabelsFromParents(const std::vector<int>& parents)
+{
+  // The values of the 'parents' array indicate the linear index of the pixel that is the parent of each pixel.
+  // Compare to the LABELS output of [LABELS CLUSTERS] = VL_FLATMAP(MAP) in Matlab.
+  std::vector<int> labels(parents.size(), 0);
+
+  // Initialize by copying
+  for(unsigned int i = 0; i < parents.size(); ++i)
+    {
+    labels[i] = parents[i];
+    }
+  
+  std::vector<int> tempLabels(parents.size(), 0);
+  std::vector<int> oldLabels(parents.size(), 0);
+  
+  while(!VectorsIdential(oldLabels, labels))
+    {
+    oldLabels = labels;
+    for(unsigned int i = 0; i < parents.size(); ++i)
+      {
+      //std::cout << "Replacing " << labels[i] << " with " << labels[labels[i]] << std::endl;
+      tempLabels[i] = labels[labels[i]]; // Replace my label with my parents label.
+      }
+    labels = tempLabels;
+    }
+
+  return labels;
+}
+
+template< typename TInputImage, typename TOutputLabelImage>
+bool QuickShiftSegmentation< TInputImage, TOutputLabelImage>
+::VectorsIdential(const std::vector<int>& v1, const std::vector<int>& v2)
+{
+  if(v1.size() != v2.size())
+    {
+    std::cerr << "Vectors must be the same size!" << std::endl;
+    exit(-1);
+    }
+  for(unsigned int i = 0; i < v1.size(); ++i)
+    {
+    if(v1[i] != v2[i])
+      {
+      return false;
+      }
+    }
+  return true;
+}
+
+template< typename TInputImage, typename TOutputLabelImage>
+std::vector<int> QuickShiftSegmentation< TInputImage, TOutputLabelImage>
+::SequentialLabels(const std::vector<int>& v)
