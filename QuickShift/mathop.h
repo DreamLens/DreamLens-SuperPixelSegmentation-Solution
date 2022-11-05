9fa8bfad81a47927aad9bef41ae51f8ba0ad6d71
DreamLens <DreamLen98@proton.me>
2022-11-05 21:35:58 +0000

    Another post migrated

--- QuickShift/mathop.h
+++ QuickShift/mathop.h
@@ -72,3 +72,112 @@ static union { vl_uint64 raw ; double value ; }
 #ifdef VL_COMPILER_MSC
     { 0x7FF0000000000000ui64 } ;
 #else
+    { 0x7FF0000000000000ULL } ;
+#endif
+
+/** @brief IEEE single precision NaN (not signaling) */
+#define VL_NAN_F (vl_nan_f.value)
+
+/** @brief IEEE single precision positive infinity (not signaling) */
+#define VL_INFINITY_F (vl_infinity_f.value)
+
+/** @brief IEEE double precision NaN (not signaling) */
+#define VL_NAN_D (vl_nan_d.value)
+
+/** @brief IEEE double precision positive infinity (not signaling) */
+#define VL_INFINITY_D (vl_infinity_d.value)
+
+/* ---------------------------------------------------------------- */
+
+/** @brief Fast <code>mod(x, 2 * VL_PI)</code>
+ **
+ ** @param x input value.
+ ** @return <code>mod(x, 2 * VL_PI)</code>
+ **
+ ** The function is optimized for small absolute values of @a x.
+ **
+ ** The result is guaranteed to be not smaller than 0. However, due to
+ ** finite numerical precision and rounding errors, the result can be
+ ** equal to 2 * VL_PI (for instance, if @c x is a very small negative
+ ** number).
+ **/
+
+VL_INLINE float
+vl_mod_2pi_f (float x)
+{
+  while (x > (float)(2 * VL_PI)) x -= (float) (2 * VL_PI) ;
+  while (x < 0.0F) x += (float) (2 * VL_PI);
+  return x ;
+}
+
+/** @brief Fast <code>mod(x, 2 * VL_PI)</code>
+ ** @see vl_mod_2pi_f
+ **/
+
+VL_INLINE double
+vl_mod_2pi_d (double x)
+{
+  while (x > 2.0 * VL_PI) x -= 2 * VL_PI ;
+  while (x < 0.0) x += 2 * VL_PI ;
+  return x ;
+}
+
+/** @brief Fast <code>(int) floor(x)</code>
+ ** @param x argument.
+ ** @return @c (int) floor(x)
+ **/
+
+VL_INLINE int
+vl_floor_f (float x)
+{
+  int xi = (int) x ;
+  if (x >= 0 || (float) xi == x) return xi ;
+  else return xi - 1 ;
+}
+
+/** @brief Fast <code>(int) floor(x)</code>
+ ** @see vl_floor_f
+ **/
+
+VL_INLINE int
+vl_floor_d (double x)
+{
+  int xi = (int) x ;
+  if (x >= 0 || (double) xi == x) return xi ;
+  else return xi - 1 ;
+}
+
+/** @brief Fast @c abs(x)
+ ** @param x argument.
+ ** @return @c abs(x)
+ **/
+
+VL_INLINE float
+vl_abs_f (float x)
+{
+#ifdef VL_COMPILER_GNU
+  return __builtin_fabsf (x) ;
+#else
+  return fabsf(x) ;
+#endif
+}
+
+/** @brief Fast @c abs(x)
+ ** @sa vl_abs_f
+ **/
+
+VL_INLINE double
+vl_abs_d (double x)
+{
+#ifdef VL_COMPILER_GNU
+  return __builtin_fabs (x) ;
+#else
+  return fabs(x) ;
+#endif
+}
+
+VL_INLINE double
+vl_log2_d (double x)
+{
+#ifdef VL_COMPILER_GNU
+  return __builtin_log2(x) ;
