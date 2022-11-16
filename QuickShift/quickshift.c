c000eb44151cd63d19952c2991013662df3f295d
DreamLens <DreamLen98@proton.me>
2022-11-16 17:47:26 +0000

    Add nvm

--- QuickShift/quickshift.c
+++ QuickShift/quickshift.c
@@ -2,4 +2,10 @@
  ** @file     quickshift.c
  ** @author   Brian Fulkerson
  ** @author   Andrea Vedaldi
- ** @brief    Quick shift image segmentation - D
+ ** @brief    Quick shift image segmentation - Definition
+ **/
+
+/* AUTORIGHTS
+Copyright (C) 2007-10 Andrea Vedaldi and Brian Fulkerson
+
+This 
