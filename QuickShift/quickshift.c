2e2ce2db08ed169023f4a87228325085f057ffd8
DreamLens <DreamLen98@proton.me>
2022-11-19 19:29:29 +0000

    0.1.8

--- QuickShift/quickshift.c
+++ QuickShift/quickshift.c
@@ -8,4 +8,8 @@
 /* AUTORIGHTS
 Copyright (C) 2007-10 Andrea Vedaldi and Brian Fulkerson
 
-This 
+This file is part of VLFeat, available under the terms of the
+GNU GPLv2, or (at your option) any later version.
+*/
+
+/** 
