34c8e5a487b656759766184fe00e1a09dbe80195
DreamLens <DreamLen98@proton.me>
2023-04-16 11:34:52 +0000

    fixed animation

--- QuickShift/quickshift.h
+++ QuickShift/quickshift.h
@@ -109,3 +109,102 @@ vl_quickshift_get_max_dist (VlQS const *q)
  ** @return the standard deviation of the kernel used in the Parzen density
  **         estimate.  
  **/
+
+VL_INLINE vl_qs_type
+vl_quickshift_get_kernel_size (VlQS const *q) 
+{
+  return q->sigma ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Get medoid.
+ ** @param q quick Shift object.
+ ** @return @c true if medoid shift is used instead of quick shift.
+ **/
+
+VL_INLINE vl_bool
+vl_quickshift_get_medoid (VlQS const *q) 
+{
+  return q->medoid ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Get parents.
+ ** @param q quick shift object.
+ ** @return a height x width matrix containing linearized links to the
+ **         parents in the quick shift tree. If there is no parent, the
+ **         parent will be set to the index of the node.
+ **/
+
+VL_INLINE int *
+vl_quickshift_get_parents (VlQS const *q) 
+{
+  return q->parents ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Get dists.
+ ** @param q quick shift object.
+ ** @return for each pixel, the distance in feature space to the pixel
+ **         that is its parent in the quick shift tree.
+ **/
+
+VL_INLINE vl_qs_type *
+vl_quickshift_get_dists (VlQS const *q) 
+{
+  return q->dists ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Get density.
+ ** @param q quick shift object.
+ ** @return the estimate of the density at each pixel.
+ **/
+
+VL_INLINE vl_qs_type *
+vl_quickshift_get_density (VlQS const *q) 
+{
+  return q->density ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Set sigma
+ ** @param q quick shift object.
+ ** @param sigma standard deviation of the kernel used in the Parzen density
+ **        estimate.  
+ **/
+
+VL_INLINE void
+vl_quickshift_set_kernel_size (VlQS *q, vl_qs_type sigma) 
+{
+  q -> sigma = sigma ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Set max distance 
+ ** @param q quick shift object.
+ ** @param tau the maximum distance in the feature space between nodes in the
+ **            quick shift tree.  
+ **/
+
+VL_INLINE void
+vl_quickshift_set_max_dist (VlQS *q, vl_qs_type tau) 
+{
+  q -> tau = tau ;
+}
+
+/** ------------------------------------------------------------------
+ ** @brief Set medoid
+ ** @param q quick shift object.
+ ** @param medoid @c true to use kernelized medoid shift, @c false (default) uses
+ **        quick shift.
+ **/
+
+VL_INLINE void
+vl_quickshift_set_medoid (VlQS *q, vl_bool medoid) 
+{
+  q -> medoid = medoid ;
+}
+
+
+#endif
