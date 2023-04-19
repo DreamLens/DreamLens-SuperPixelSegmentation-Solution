00ee95b8200826d616926e061a8ebb792db9c3e5
DreamLens <DreamLen98@proton.me>
2023-04-19 22:51:08 +0000

    change requirements

--- QuickShift/quickshiftExample.cpp
+++ QuickShift/quickshiftExample.cpp
@@ -5,4 +5,9 @@
 #include "itkQuickShiftSegmentation.h"
 
 typedef itk::VectorImage<unsigned char, 2> ImageType;
-typedef itk::Image<unsigned int, 2> 
+typedef itk::Image<unsigned int, 2> LabelImageType;
+
+template<typename TImage>
+static void CreateImage(TImage* image);
+
+template<typ
